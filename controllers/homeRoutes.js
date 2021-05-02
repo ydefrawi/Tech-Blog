04a30812e31e9e4278ff2767f7ef8cf7c89d3167
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-02 18:29:07 -0500

    got my data to seed. However, now the dashboard page is not loading. Likely a routing issue.

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -50,7 +50,7 @@ router.get('/post/:id', async (req, res) => {
 });
 
 // Use withAuth middleware to prevent access to route
-router.get('/profile', withAuth, async (req, res) => {
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
     // Find the logged in user based on the session ID
     const userData = await User.findByPk(req.session.user_id, {
