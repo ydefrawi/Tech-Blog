9d51f5831a55097404e9b82b0256ebb245937f83
Yousef Defrawi <76498767+ydefrawi@users.noreply.github.com>
2021-05-02 17:32:53 -0500

    commit for debugging

--- models/Post.js
+++ models/Post.js
@@ -3,7 +3,7 @@ const sequelize = require('../config/connection');
 
 class Project extends Model {}
 
-Project.init(
+Post.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -23,10 +23,6 @@ Project.init(
       allowNull: false,
       defaultValue: DataTypes.NOW,
     },
-    needed_funding: {
-      type: DataTypes.FLOAT,
-      allowNull: false,
-    },
     user_id: {
       type: DataTypes.INTEGER,
       references: {
@@ -40,8 +36,8 @@ Project.init(
     timestamps: false,
     freezeTableName: true,
     underscored: true,
-    modelName: 'project',
+    modelName: 'post',
   }
 );
 
-module.exports = Project;
+module.exports = Post;
--- utils/helpers.js
+++ utils/helpers.js
@@ -19,4 +19,16 @@ module.exports = {
       return `<span for="img" aria-label="gear">⚙️</span>`;
     }
   },
+  get_image: () => {
+    const randomNum = Math.random();
+
+    // Return a random emoji
+    if (randomNum > 0.7) {
+      return `<img class="blog-thumb" url="https://images.unsplash.com/photo-1527685216219-c7bee79b0089?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1934&q=80">`;
+    } else if (randomNum > 0.4) {
+      return `<img class="blog-thumb" url="https://images.unsplash.com/photo-1518770660439-4636190af475?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80">`;
+    } else {
+      return `<img class="blog-thumb" url="https://images.unsplash.com/photo-1518770660439-4636190af475?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80">`;
+    }
+  },
 };
